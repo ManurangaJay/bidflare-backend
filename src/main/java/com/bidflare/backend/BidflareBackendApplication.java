20443580a53342f2c9b92e758d2f6587690cef08
Manuranga <manurangajayawardhana@gmail.com>
2025-07-05 17:08:12 +0530

    init: initialize the project

---
+++ src/main/java/com/bidflare/backend/BidflareBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bidflare.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidflareBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BidflareBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bidflare/backend/BidflareBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bidflare.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidflareBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
